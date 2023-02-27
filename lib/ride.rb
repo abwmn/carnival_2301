5ebcb4405b1bdb79728649263992dc6ebd17b2c0
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-27 11:49:46 -0500

    feat: ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,28 @@
 class Ride
- 
+  attr_reader :name, 
+              :min_height, 
+              :admission_fee, 
+              :excitement, 
+              :total_revenue, 
+              :rider_log
+
+  def initialize(details)
+    @name = details[:name]
+    @min_height = details[:min_height]
+    @admission_fee = details[:admission_fee]
+    @excitement = details[:excitement]
+    @total_revenue = 0
+    @rider_log = Hash.new(0)
+  end
+
+  def board_rider(visitor)
+    return false if visitor.height < @min_height ||
+                    !visitor.preferences.include?(@excitement) ||
+                    visitor.spending_money < @admission_fee
+
+    @total_revenue += @admission_fee
+    @rider_log[visitor] += 1
+    visitor.spending_money -= @admission_fee
+    true
+  end
 end
