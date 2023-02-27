a24b110364091f304f399e981ca72bb672544368
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-27 11:49:37 -0500

    initial commit

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,19 @@
 class Visitor
+  attr_reader :height, :name, :preferences
+  attr_accessor :spending_money
   
+  def initialize(name, height, money)
+    @height = height
+    @name = name
+    @preferences = []
+    @spending_money = money.gsub('$', '').to_i
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(height_requirement)
+    @height >= height_requirement
+  end
 end
